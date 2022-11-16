30c0da22d2da248e31ba9a0b915f41e57c9c236a
Jane <jane@MacBook-Pro2.local>
2022-11-16 19:44:37 +0300

    add getBalace

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"balance/internal/db"
 	"balance/internal/config"
+	"balance/internal/handler"
+	//"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 	"net/http"
 	"log"
 	"fmt"
@@ -14,18 +17,24 @@ func main() {
 		log.Fatal(err)
 	}
 
-    // http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
-    //     fmt.Fprintf(w, "Hello!")
-    // })
+    http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Hello!")
+    })
 
-	pool, err := db.CreatePool(&c)
+	db, err := db.ConnectDB(&c)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer pool.Close()
+	defer db.Close()
 
-	fmt.Printf("Starting server at port 8080\n")
+	fmt.Printf("Starting server at port 8080")
 
+    http.HandleFunc("/balance", handler.GetBalance)
+
+	// router := mux.NewRouter()
+	// //router.HandleFunc("/add", handler.AddMoney).Methods("POST")
+	// router.HandleFunc("/balance", handler.GetBalance).Methods("GET")
+	
 	if err := http.ListenAndServe(":8080", nil); err != nil {
         log.Fatal(err)
     }
